a82485d8e0296d740ab0d8ac1dcb79d982484580
Bitcoin Buble <bitcoin.buble@gmail.com>
2019-05-31 14:26:52 +0100

    throw in styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Crypto Weekly`,
+    description: `The UK’s biggest and best weekly cryptocurrency podcast`,
+    author: `@bitcoin_buble`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffbd3a`,
+        theme_color: `#ffbd3a`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,32 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.header`
+  background-color: #ffbd3a;
+  margin-bottom: 1.45rem;
+  font-family: "Ultra";
+
+  > div {
+    margin: 0 auto;
+    max-width: 960px;
+    padding: 1.45rem 1.0875rem;
+  }
+
+  a {
+    color: #111111;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <Container>
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
-  </header>
+  </Container>
 )
 
 Header.propTypes = {
