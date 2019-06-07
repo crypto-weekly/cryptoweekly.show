d372f5d6bde6eb569aabfec1b2999f334f73ca48
Bitcoin Buble <bitcoin.buble@gmail.com>
2019-06-07 16:07:29 +0100

    use crow as favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
         background_color: `#ffbd3a`,
         theme_color: `#ffbd3a`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/crow.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
